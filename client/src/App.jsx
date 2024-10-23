9fb114fab6870aff903978d26359d09974e602f6
TOM SABU <tomsabu0000@gmail.com>
2024-10-23 16:42:54 +0530

    PostCSS and Tailwind configuration files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
