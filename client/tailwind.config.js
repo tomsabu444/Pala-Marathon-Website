fb4b78f13212f690f1931cdd864dfe6ffbe9866d
TOM SABU <tomsabu0000@gmail.com>
2024-10-31 16:29:06 +0530

    Add registration page and update Tailwind configuration and Navbar postion changed
    - Introduced RegisterPage component for user registration
    - Updated Tailwind config with new custom colors and font family
    - Added text shadow utilities for enhanced text styling
    - Replaced favicon and updated document title in index.html
    - Removed unused vite.svg assets

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,12 +4,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        custom: {
-          'purple-1000': '#330A48',
-          'purple-1001': '#330A48'
+        'custom-purple-1000': '#330A48',
+        'custom-purple-1001': '#330A48',
+        'custom-pink': '#8D1455',
       },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow': {
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)',
+        },
+        '.text-shadow-md': {
+          textShadow: '3px 3px 6px rgba(0, 0, 0, 0.6)',
+        },
+        '.text-shadow-lg': {
+          textShadow: '4px 4px 8px rgba(0, 0, 0, 0.7)',
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
