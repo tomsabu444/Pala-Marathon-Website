977c63247e4549715fa79bdfe1289777f58fc101
TOM SABU <tomsabu0000@gmail.com>
2024-11-03 11:56:31 +0530

    Improved dateOfBirth validation to ensure users are at least 12 years old. Updated error messages for gender and medical questions. This enhances data accuracy and user guidance.

--- client/src/Schema/Schemas.js
+++ client/src/Schema/Schemas.js
@@ -2,27 +2,36 @@ import { z } from "zod";
 
 const basicDetailsSchema = z.object({
   name: z.string().min(1, "Name is required"),
-  phone: z
-    .string()
-    .regex(/^\+91\d{10}$/, "Phone number should be +91xxxxxxxxxx"),
+  phone: z.string().regex(/^\+91\d{10}$/, "Phone number be +91xxxxxxxxxx"),
   email: z.string().email("Invalid email address"),
   gender: z
     .enum(["male", "female", "other"], {
       required_error: "Please select your gender",
-      invalid_type_error: "Please select your gender",
+      invalid_type_error: "Please select a valid gender option",
     })
     .refine((val) => val !== undefined && val !== null, {
       message: "Gender selection is required",
     }),
-  dateOfBirth: z.string().refine(
-    (date) => {
-      const currentDate = new Date();
-      const birthDate = new Date(date);
-      const age = currentDate.getFullYear() - birthDate.getFullYear();
-      return age >= 12;
-    },
-    { message: "Participant must be at least 12 years old" }
-  ),
+  dateOfBirth: z
+    .string()
+    .min(1, "Date of Birth is required")
+    .refine(
+      (date) => {
+        const currentDate = new Date();
+        const birthDate = new Date(date);
+        const age = currentDate.getFullYear() - birthDate.getFullYear();
+        // Check if the user is at least 12 years old
+        return (
+          age > 12 ||
+          (age === 12 &&
+            currentDate >= birthDate.setFullYear(birthDate.getFullYear() + 12))
+        );
+      },
+      {
+        message: "Participant must be at least 12 years old",
+      }
+    ),
+
   address: z.object({
     line1: z.string().min(1, "Address line 1 is required"),
     city: z.string().min(1, "City is required"),
@@ -41,30 +50,40 @@ const medicalDetailsSchema = z.object({
     relation: z.string().min(1, "Relation with Emergency Contact is required"),
     contactNumber: z
       .string()
-      .regex(/^\+91\d{10}$/, "Emergency Contact Number must be +91xxxxxxxxxx"),
+      .regex(
+        /^\+91\d{10}$/,
+        "Emergency Contact Number Phone number be +91xxxxxxxxxx"
+      ),
   }),
   medicalInfo: z.string().optional(),
   questions: z.object({
     heartCondition: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "Please select yes or no",
     }),
     chestPainActivity: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "This question is required",
     }),
     chestPainRest: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "This question is required",
     }),
     dizziness: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "This question is required",
     }),
     boneOrJointProblem: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "This question is required",
     }),
     bloodPressureMedication: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "This question is required",
     }),
     otherReason: z.enum(["yes", "no"], {
       required_error: "This question is required",
+      invalid_type_error: "This question is required",
     }),
   }),
 });
