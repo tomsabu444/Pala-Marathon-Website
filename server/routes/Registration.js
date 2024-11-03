bbebec6d67f046b5f33ba8e2e0745a325f5107d9
TOM SABU <tomsabu0000@gmail.com>
2024-11-03 19:18:55 +0530

    Implement registration endpoint to save user data

--- server/routes/Registration.js
+++ server/routes/Registration.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Registration = require('../models/RegistrationShema');
 
-// Endpoint to save registration data
 router.post('/register', async (req, res) => {
   try {
-    const registrationData = req.body; // Data from the frontend
+    const registrationData = req.body; 
     const registration = new Registration(registrationData);
     await registration.save();
     res.status(201).json({ message: 'Registration saved successfully!' });
