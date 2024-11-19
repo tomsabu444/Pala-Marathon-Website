bdba6b2bc9449dc9809137de9a4f607b249eddf5
Jishnu <jishnumohan292@gmail.com>
2024-11-19 23:02:24 +0530

    commit

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -36,7 +36,7 @@ function ContactPage() {
         <img src={rectangle_bar} alt="Rectangle Bar" className="w-full h-full" />
       </div>
 
-      <div className="flex flex-col lg:flex-row gap-6 lg:gap-10 p-4 sm:p-6 lg:p-20 max-w-7xl mx-auto">
+      <div className="flex flex-col lg:flex-row gap-6 lg:gap-44 p-4 sm:p-6 lg:p-20 mx-auto">
         {/* Map Section */}
         <div className="flex justify-center items-center md:w-full">
           <div className="relative">
