483d893b7630bc69c59b45bbd243450c4c4a7c7b
TOM SABU <tomsabu0000@gmail.com>
2024-11-20 09:20:52 +0530

    Refactor HomePage component and add SVG pattern
    Update HomePage component to use consistent import statements and improve code readability
    Add SVG pattern to enhance visual design

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import ContactPage from './ContactPage'
-import GoverningBodyPage from './GoverningBodyPage'
+import React from "react";
+import ContactPage from "./ContactPage";
+import GoverningBodyPage from "./GoverningBodyPage";
 
 import PalaMarathon from "../assets/PalaMarathon.svg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
+//? svg pattern
+import dots_Pattern from "../assets/dots_Pattern.svg";
 
 function HomePage() {
   return (
     <>
-    <div className='h-screen bg-gradient-to-b from-[#FFC1E2] via-[#FFC1E2] to-[#FFFFFF] font-outfit'>
-      <div className='flex flex-col items-center justify-center gap-3 h-full'>
-      <img
+      <div className=" relative h-screen bg-gradient-to-b from-[#FFC1E2] via-[#FFC1E2] to-[#FFFFFF] font-outfit">
+        {/*//! Dots Pattern */}
+        <div className="hidden lg:block absolute top-44 ">
+          <img src={dots_Pattern} alt="Dots Pattern" />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-3 h-full">
+          <img
             src={PalaMarathon}
             alt="PalaMarathon"
             className="h-40 mb-2 drop-shadow-lg"
           />
-          <h1 className='text-xl font-bold text-[#8B0A1E]'>19th JANUARY 2025</h1>
-          <Link to='/register' className='px-8 py-3 mt-4 text-white bg-[#330A48] rounded-full'>REGISTER NOW</Link>  
+          <h1 className="text-xl font-bold text-[#8B0A1E]">
+            19th JANUARY 2025
+          </h1>
+          <Link
+            to="/register"
+            className="px-8 py-3 mt-4 text-white bg-[#330A48] rounded-full"
+          >
+            REGISTER NOW
+          </Link>
+        </div>
       </div>
-    </div>
 
-    {/* Governing Body */}
-    <GoverningBodyPage/>
-    
-    {/* Contact US */}
-    <ContactPage />
+      {/* Governing Body */}
+      <GoverningBodyPage />
+
+      {/* Contact US */}
+      <ContactPage />
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
