2ebc2ef6b8e93713da47d99c23e6e67b83c59814
TOM SABU <tomsabu0000@gmail.com>
2024-11-21 19:45:59 +0530

    Update CountdownTimer responsiveness and center align Governing Body title

--- client/src/pages/GoverningBodyPage.jsx
+++ client/src/pages/GoverningBodyPage.jsx
@@ -26,7 +26,7 @@ export default function GoverningBodyPage() {
 
   return (
     <div className="flex font-outfit flex-col items-center w-full p-16">
-      <h1 className="text-3xl  md:text-4xl font-semibold mb-14">GOVERNING BODY</h1>
+      <h1 className="text-3xl text-center  md:text-4xl font-semibold mb-14">GOVERNING BODY</h1>
       <div className="flex flex-col md:flex-row justify-evenly gap-12 lg:w-5/6">
         {governingBodyMembers.map((member, index) => (
           <div key={index} className="flex flex-col items-center text-center">
