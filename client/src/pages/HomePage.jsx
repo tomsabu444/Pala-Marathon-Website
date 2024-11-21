d49913d77dc48e6ccbba217107c9271fb9098e8c
TOM SABU <tomsabu0000@gmail.com>
2024-11-21 11:12:41 +0530

    Refactor homepage layout and add dots pattern
    Improve the layout of the homepage by refactoring the code and adding dots pattern on both sides. This enhances the visual appeal and provides a more balanced design.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,32 +16,36 @@ function HomePage() {
   return (
     <>
       <div className="relative h-dvh bg-gradient-to-b from-[#FFC1E2] via-[#FFC1E2] to-[#FFFFFF] font-outfit">
-        {/*//! Dots Pattern */}
+        {/*//! Dots Pattern left */}
         <div className="absolute top-24">
+          <img src={dots_Pattern} alt="Dots Pattern" />
+        </div>
+          {/*//! Dots Pattern right */}
+          <div className=" hidden sm:block absolute right-0 bottom-14 rotate-180">
           <img src={dots_Pattern} alt="Dots Pattern" />
         </div>
 
         {/* //! contact No */}
         <div className=" hidden md:block absolute bottom-24 -left-16 transform -rotate-90 text-[#330A48] transform-origin-left">
-          <p className="text-lg">Contact No: 9784586721</p>
+          <p className="text-lg font-light">Contact No: <span className="font-medium">9784586721  </span> </p>
         </div>
 
         {/* //! Main Content */}
         <div className="lg:flex lg:justify-between h-full">
-          <div className="flex flex-col justify-items-center h-full lg:pl-44">
+          <div className="flex relative z-20 flex-col justify-items-center h-full lg:pl-20 xl:pl-44">
             <div className="flex flex-col items-center justify-center mt-10 pt-16 gap-4 h-4/6 ">
               <img
                 src={PalaMarathon}
                 alt="PalaMarathon"
-                className="h-40 md:h-44 lg:h-52 mb-2 drop-shadow-lg"
+                className="h-40 md:h-48 lg:h-52 mb-2 drop-shadow-lg"
               />
-              <h1 className="text-xl lg:text-2xl font-bold text-[#8B0A1E]">
+              <h1 className="text-xl md:text-2xl font-bold text-[#8B0A1E]">
                 19th JANUARY 2025
               </h1>
 
               <Link
                 to="/register"
-                className="px-8 py-3 mt-4 text-white bg-[#330A48] rounded-full lg:text-xl"
+                className="px-8 py-3 mt-4 text-white bg-[#330A48] rounded-full md:text-xl"
               >
                 REGISTER NOW
               </Link>
@@ -59,8 +63,8 @@ function HomePage() {
           </div>
 
           {/* img container */}
-          <div className=" hidden max-w-6xl lg:block lg:pt-44">
-          <img src={bg_img_homepage}/>
+          <div className=" hidden lg:flex absolute  right-0 bottom-5">
+          <img src={bg_img_homepage} className=" lg:w-[1100px]"/>
           </div>
         </div>
       </div>
