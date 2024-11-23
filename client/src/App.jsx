d988667baa20a6f3c930a95fbbaba75402336315
Jishnu <jishnumohan292@gmail.com>
2024-11-24 01:04:15 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
@@ -11,48 +11,22 @@ import ScrollToTop from "./utils/ScrollToTop";
 import AboutUS from "./pages/AboutUS";
 import NotFoundPage from "./pages/NotFoundPage";
 
-// Layout with both Navbar and Footer
-const MainLayout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
-  );
-};
-
-// Layout with only Navbar
-const NavOnlyLayout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-};
-
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <Navbar />
       <Routes>
-        {/* Main layout routes */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          {/* <Route path="/register" element={<RegisterPage />} /> */}
-          <Route path="/terms-conditions" element={<TermsConditionsPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="/disclaimer" element={<DisclaimerPage />} />
-          <Route path="/about-us" element={<AboutUS />} />
-        </Route>
-        
-        {/* Routes with only Navbar */}
-        <Route element={<NavOnlyLayout />}>
-          <Route path="/404" element={<NotFoundPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+
+        <Route path="/terms-conditions" element={<TermsConditionsPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/disclaimer" element={<DisclaimerPage />} />
+        <Route path="/about-us" element={<AboutUS />} />
+        <Route path="/404" element={<NotFoundPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
